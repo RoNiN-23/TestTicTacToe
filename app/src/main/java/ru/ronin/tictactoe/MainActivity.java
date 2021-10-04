9a65b2a15ead08de65769db72f32440c7067d5bb
Egor Smirnov <smegr@mail.ru>
2021-10-05 01:09:55 +0300

    Создание игрового поля

--- app/src/main/java/ru/ronin/tictactoe/MainActivity.java
+++ app/src/main/java/ru/ronin/tictactoe/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private TableLayout tableLayout;
+    private Button[][] buttons = new Button[3][3];
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tableLayout = findViewById(R.id.tableLayout);
+        buildGameField(); // создание игрового поля
+    }
+
+    private void buildGameField()
+    {
+        Square[][] field = game.getField();
+        for (int i = 0, lenI = field.length; i < lenI; i++ ) {
+            TableRow row = new TableRow(this); // создание строки таблицы
+            for (int j = 0, lenJ = field[i].length; j < lenJ; j++)
+            {
+                Button button = new Button(this);
+                buttons[i][j] = button;
+                button.setOnClickListener(new Listener(i, j));
+                row.addView(button, new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT)); // добавление кнопки в строку таблицы
+                button.setWidth(160);
+                button.setHeight(160);
+            }
+            tableLayout.addView(row,
+                    new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT)); // добавление строки в таблицу
+        }
+    }
+
+    public class Listener implements View.OnClickListener
+    {
+        private int x = 0;
+        private int y = 0;
+
+        Listener(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void onClick(View view)
+        {
+            Button button = (Button) view;
+        }
     }
 }
